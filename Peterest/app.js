6ebf6ad1f5d4d4b3b1acda2a4cea7f0758d12846
theodosiskrt <76492551+theodosiskrt@users.noreply.github.com>
2021-05-23 00:21:51 +0300

    Add files via upload
    Added a Home Page

--- Peterest/app.js
+++ Peterest/app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const ejsMate = require('ejs-mate');
-const post = require('./models/post.js');
 const path = require('path');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
-const catchAsync = require('./utilities/catchAsync.js');
-const postJoiSchema = require('./joiSchemas/post.js')
 const ExpressError = require('./utilities/errorHandler.js');
 const app = express();
 
@@ -18,84 +15,39 @@ mongoose.connect('mongodb://localhost:27017/Peterest', {useNewUrlParser:true, us
         console.log('Error Connecting to Database, exiting...');
         process.exit();
     })
-const validatePost = (req, res, next) => {
-    const {error} = postJoiSchema.validate(req.body);
-    if(error) return next(error);
-    return next();
-}
 
+const postsRoutes = require('./routes/postsRouts.js')
+app.use('/posts', postsRoutes);
 
 //Using Styles and Scripts on the Templates
-app.use(express.static('assets'));
-//Using Styles and Scripts when running the server from any directory
-app.set('assets', path.join(__dirname, '/assets'))
+app.use(express.static(path.join(__dirname, 'assets')));
 //Ejs Engine
 app.engine('ejs', ejsMate);
 //Using EJS
 app.set('view engine', 'ejs');
 //Running the server from every folder
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'views'));
 //To get data from forms
 app.use(express.urlencoded({extended: true}));
 //Using Method Override to create Patch requests with forms
 app.use(methodOverride('_method'));
 
-//Home Page
-app.get('/', catchAsync(async (req, res) => {
-    const posts = await post.find({});
-    posts.reverse();
-    res.render('Peterest/posts.ejs', {posts});
-}))
 
-//Create post Form
-app.get('/posts/new', (req, res) => {
-    res.render('Peterest/new.ejs');
+//Home Page
+app.get('/', (req, res) => {
+    res.render('home');
 })
 
-//Show Single post
-app.get('/posts/:id', catchAsync(async (req, res) => {
-    const {id} = req.params;
-    const fPost = await post.findById(id);
-    res.render('Peterest/show.ejs', {post: fPost});
-}))
-
-//Edit Tweet Form
-app.get('/posts/:id/edit', catchAsync(async (req, res) => {
-    const {id} = req.params;
-    const fPost = await post.findById(id);
-    res.render('Peterest/edit.ejs', {post: fPost});
-}))
-
-//Update post with the data collected from the Edit Form
-app.patch('/posts/:id', validatePost, catchAsync(async(req, res) => {
-    const {id} = req.params;
-    await post.findByIdAndUpdate(id, req.body, {runValidators: true, useFindAndModify:false});
-    res.redirect(`/`)
-}))
-
-//Delete post
-app.delete('/posts/:id', catchAsync(async (req, res) => {
-    const {id} = req.params;
-    await post.findByIdAndDelete(id);
-    res.redirect('/');
-}))
-
-//Create post
-app.post('/posts', validatePost, catchAsync(async (req, res) => {
-    const newPost = new post(req.body)
-    await newPost.save();
-    res.redirect(`/`)
-}))
-
+//Page Not Found
 app.all('*', (req, res, next) => {
     return next(new ExpressError(404, 'Page Not Found'));
-})
-
+});
 app.use((error, req, res, next) => {
+    if(!error.message) error.message = 'Oh no something went wrong!';
     res.render('error.ejs', {error});
-})
+});
 
 //Listen on Port 3000
 app.listen(3000, () => {
     console.log('Listening on Port 3000');
-})
+});
