a0a98d72f4d1c35986f0304f03fb4ae02bd74420
theodosiskrt <76492551+theodosiskrt@users.noreply.github.com>
2021-05-28 18:00:18 +0300

    Delete Peterest directory

--- Peterest/joiSchemas/post.js
+++
@@ -1,7 +0,0 @@
-const joi = require('joi');
-
-module.exports = joi.object({
-    title: joi.string().required(),
-    body: joi.string().required(),
-    image:joi.string().allow('')
-})
--- Peterest/utilities/errorHandler.js
+++
@@ -1,9 +0,0 @@
-class ExpressError extends Error{
-    constructor(status, message){
-        super();
-        this.message = message;
-        this.status = status;
-    }
-}
-
-module.exports = ExpressError;
